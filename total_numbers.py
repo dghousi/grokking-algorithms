3c977061006529e20b11b369c126f4965d36cb43
dghousi <dawlatzai.ghousi@gmail.com>
2025-03-02 13:24:21 +0430

    add comment for arr[1:]

--- total_numbers.py
+++ total_numbers.py
@@ -4,12 +4,15 @@ def sum(arr):
         total +=x
     return total
 
-print(sum([1,2,3,4]))
+print(sum([3, 5, 7, 9]))
 
 def recursiveSum(arr):
     # Base case: if the array is empty, return 0
     if not arr:
         return 0
     # Recursive case: sum the first element and the sum of the rest of the array
+    # arr[1:]: This slice takes all elements of the list arr starting from index 1 to the end of the list.
+    # So, if arr is [3, 5, 7, 9], then:
+    # arr[1:] would result in [5, 7, 9].
     return arr[0] + recursiveSum(arr[1:])
-print(recursiveSum([1,2,3,4]))
+print(recursiveSum([3, 5, 7, 9]))
